c9f373f61991fd327d770f886ee812e98133f339
Logan Gregg <logan@DESKTOP-BJDHKUJ.localdomain>
2024-02-29 10:12:02 -0600

    Finished Symbol Table assignment

--- sym_table/SymTable.cpp
+++ sym_table/SymTable.cpp
@@ -6,64 +6,148 @@
 // For this assignment you are to write a module that will maintain a symbol table for your compiler.  Use the linked implementation of a hash table.  The unit should provide the following operations which must be named exactly as listed below:
 
 // Constructor & Destructor
-SymbolTable::SymbolTable()
+SymbolTable::SymbolTable() : m_table(TABLE_SIZE)
 {
-    m_table = std::vector<std::list<TableRecord *>>(TABLE_SIZE);
-    
+
 }
 
 SymbolTable::~SymbolTable()
 {
-    for (auto& list : m_table)
+    for (int i = 0; i < m_table.size(); i++) 
     {
-            for (auto & record : list) 
-            {
-                delete record;
-            }
-
-            list.clear();
+        TableRecord* curr_node = m_table[i];
+        
+        while (curr_node != nullptr)
+        {
+            TableRecord* temp = curr_node;
+            curr_node = curr_node->m_next;
+            delete temp;
+        }
     }
 }
 
 // Insert(lex,token,depth) - insert the lexeme, token and depth into a record in the symbol table.
-void SymbolTable::Insert(std::string lexeme, Token token, int depth)
+void SymbolTable::Insert(std::string p_lexeme, Token p_token, int p_depth)
 {
-    // unsigned int hash = hash(lexeme);
+    int hash = this->hash(p_lexeme);
 
-    // TableRecord * new_entry = new TableRecord();
-    
+    TableRecord * current_node = m_table[hash];
+
+    TableRecord * new_node = new TableRecord();
+    new_node->m_lexeme = p_lexeme;
+    new_node->m_token = p_token;
+    new_node->m_depth = p_depth;
+
+    if (current_node == nullptr)
+    {
+        m_table[hash] = new_node;
+    }
+    else
+    {
+        while(current_node->m_next != nullptr)
+        {
+            current_node = current_node->m_next;
+        }
+        
+        current_node->m_next = new_node;
+    }
 }
 
 // Lookup(lex) - lookup uses the lexeme to find the entry and returns a pointer to that entry.
 TableRecord * SymbolTable::Lookup(std::string lex)
 {
-    TableRecord * ret;
+    int hash = this->hash(lex);
 
+    TableRecord* ret = m_table[hash];
+    
+    while (ret != nullptr)
+    {
+        if (ret->m_lexeme == lex)
+        {
+            return ret;
+        }
 
-    return ret;
+        ret = ret->m_next;
+    }
+
+    return nullptr;
 }
 
 // DeleteDepth(depth) - delete is passed the depth and deletes all records that are in the table at that depth.
 void SymbolTable::DeleteDepth(int depth)
 {
+    for (int i = 0; i < m_table.size(); i++) {
+        
+        TableRecord * curr_node = m_table[i];
+        TableRecord * prev_node = nullptr;
 
+        while (curr_node != nullptr)
+        {
+            if (curr_node->m_depth == depth)
+            {
+                if (prev_node == nullptr)
+                {
+                    m_table[i] = curr_node->m_next;
+                }
+                else
+                {
+                    prev_node->m_next = curr_node->m_next;
+                }
+
+                TableRecord * temp = curr_node;
+                curr_node = curr_node->m_next;
+                delete temp;
+            }
+            else
+            {
+                prev_node = curr_node;
+                curr_node = curr_node->m_next;
+            }
+        }
+    }
 }
 
 // WriteTable(depth) - include a procedure that will write out all variables (lexeme only) that are in the table at a specified depth. [ this will be useful for debugging your compiler ]
 void SymbolTable::WriteTable(int depth)
 {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        
+        TableRecord * node = m_table[i];
 
+        if(node == nullptr)
+        {
+            continue;
+        }
+
+        while (node != nullptr)
+        {
+            if (node->m_depth == depth)
+            {
+                std::cout << "Lexeme: " << node->m_lexeme << " , Depth: " << node->m_depth << std::endl;
+            }
+
+            node = node->m_next;
+         }
+    }
 }
 
 // hash(lexeme) - (private)passed a lexeme and return the location for that lexeme. (this should be an internal routine only, do not list in the interface section). 
 unsigned int SymbolTable::hash(std::string lexeme)
 {
-    unsigned int hashValue = 0;
+    std::string s = lexeme;
 
-    for (size_t i = 0; i < lexeme.length(); ++i) 
-    {
-        hashValue += (lexeme[i] * (1 << i));
+    const int PRIME = TABLE_SIZE;
+
+    unsigned int h = 0, g;
+
+    for (char c : s) {
+        h = (h << 24) + c;
+
+        if ((g = h & 0xF0000000) != 0) {
+            h = h ^ (g >> 24);
+            h ^= g;
+        }
     }
 
-    return hashValue % TABLE_SIZE;
+    return h % PRIME;
 }
--- sym_table/main.cpp
+++ sym_table/main.cpp
@@ -22,37 +22,81 @@
 
 #include "LexicalAnalyzer.h"
 #include "Parser.h"
+#include "SymTable.h"
 
 int main(int argc, char* argv[])
 {
-    std::string prog_file;
 
-    if (argc != 2)
-    {
-        std::cout << "Usage: " << argv[0] << " filename" << std::endl;
-        return 100;
-    }
-    else
-    {
-        prog_file = argv[1];
-    }
-
-    // Create instance of the lexical analyzer
-    LexicalAnalyzer* lex = new LexicalAnalyzer(prog_file);
+    // Initialize test data to test with
+    Token t1;
+    t1.m_lexeme = "cat";
+    t1.m_token = Token_T::IDENTIFIER;
     
-    while (token.m_token != Token_T::EOF_T)
-    {
-        lex->GetNextToken();
-        // lex->DisplayToken();
-    }
+    Token t2;
+    t2.m_lexeme = "dog";
+    t2.m_token = Token_T::IDENTIFIER;
+    
+    Token t3;
+    t3.m_lexeme = "lizard";
+    t3.m_token = Token_T::IDENTIFIER;
+    
+    Token t4;
+    t4.m_lexeme = "otter";
+    t4.m_token = Token_T::IDENTIFIER;
+    
+    Token t5;
+    t5.m_lexeme = "platypus";
+    t5.m_token = Token_T::IDENTIFIER;
+    
+    Token t6;
+    t6.m_lexeme = "wasp";
+    t6.m_token = Token_T::IDENTIFIER;
 
-    // Delete and ReAllocate the Lexical Analyzer to reset file pos
-    delete lex;
-    lex = new LexicalAnalyzer(prog_file);
+    // Initalize the symbol table
+    SymbolTable st = SymbolTable();
 
-    Prog();
+    std::cout << "Inserting two symbols at depth 0 and two at depth 1" << std::endl;
+    st.Insert(t1.m_lexeme, t1, 0);
+    st.Insert(t2.m_lexeme, t2, 0);
+    st.Insert(t3.m_lexeme, t3, 1);
+    st.Insert(t4.m_lexeme, t4, 1);
 
-    delete lex;
-    lex = nullptr;
+    std::cout << std::endl << "Printing out all symbols at depth 0!" << std::endl;
+    st.WriteTable(0);
+    std::cout << std::endl << "Printing out all symbols at depth 1!" << std::endl;
+    st.WriteTable(1);
+
+    std::cout << std::endl << "Deleting all symbols at depth 0!" << std::endl << std::endl;
+    st.DeleteDepth(0);
+    std::cout << "Printing out all symbols at depth 0!" << std::endl;
+    st.WriteTable(0);
+
+    std::cout << std::endl << "Printing out all symbols at depth 1!" << std::endl;
+    st.WriteTable(1);
+
+    std::cout << std::endl << "Inserting four symbols at depth 0" << std::endl;
+    st.Insert(t1.m_lexeme, t1, 0);
+    st.Insert(t2.m_lexeme, t2, 0);
+    st.Insert(t1.m_lexeme, t1, 1);
+    st.Insert(t2.m_lexeme, t2, 1);
+
+    std::cout << std::endl << "Printing out all symbols at depth 0!" << std::endl;
+    st.WriteTable(0);
+
+    std::cout << std::endl << "Deleting all symbols at depth 0!" << std::endl << std::endl;
+    st.DeleteDepth(0);
+    std::cout << "Printing out all symbols at depth 0!" << std::endl;
+    st.WriteTable(0);
+
+    std::cout << std::endl << "Inserting four symbols at depth 2" << std::endl;
+    st.Insert(t5.m_lexeme, t5, 2);
+    st.Insert(t6.m_lexeme, t6, 2);
+
+    std::cout << std::endl << "Printing out all symbols at depth 0" << std::endl;
+    st.WriteTable(0);
+    std::cout << std::endl << "Printing out all symbols at depth 1" << std::endl;
+    st.WriteTable(1);
+    std::cout << std::endl << "Printing out all symbols at depth 2" << std::endl;
+    st.WriteTable(2);
 
 }
